87a83431104868b2d13d71e925cc8ec4870a12b8
Spenser Black <spenserblack01@gmail.com>
2022-01-23 20:34:41 -0500

    Rename module (#1)

---
+++ bitio.go
@@ -0,0 +1,8 @@
+// Package bitio provides types for I/O with bits.
+package bitio
+
+const byteSize int = 8
+
+// Bit is mainly for documentation purposes, and to help clarify the expected
+// return value.
+type Bit = byte
