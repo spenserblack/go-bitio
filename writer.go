b419cd37b3d23d7f03d2c89c056e8023689af0a7
Spenser Black <spenserblack01@gmail.com>
2022-01-25 19:15:22 -0500

    Implement `WriteBit` and `Commit`

--- writer.go
+++ writer.go
@@ -29,6 +29,11 @@ func NewWriter(w io.Writer, chunkSize int) *Writer {
 // The number if bits written will be returned, which will be 0 until a chunk
 // is filled.
 func (w *Writer) WriteBit(b Bit) (written int, err error) {
+	w.bytes[w.byteIndex()] |= b << (7 - w.bitIndex())
+	// NOTE Just defining bool in var for clarity of purpose
+	if wrapped := w.incIndex(); wrapped {
+		written, err = w.Commit()
+	}
 	return
 }
 
@@ -40,5 +45,25 @@ func (w *Writer) WriteBit(b Bit) (written int, err error) {
 // The number of bits written are returned, and any error that occurred when
 // writing.
 func (w *Writer) Commit() (written int, err error) {
+	written, err = w.Write(w.bytes)
+	w.bytes = make([]byte, len(w.bytes))
 	return
+}
+
+// ByteIndex gets the index of the current byte to write bits to.
+func (w Writer) byteIndex() int {
+	return w.index / byteSize
+}
+
+// BitIndex gets the index of the current bit in the byte to write to.
+func (w Writer) bitIndex() int {
+	return w.index % byteSize
+}
+
+// IncIndex increments the index and wraps it. Returns if the number was
+// wrapped.
+func (w *Writer) incIndex() bool {
+	w.index++
+	w.index %= len(w.bytes) * byteSize
+	return w.index == 0
 }
