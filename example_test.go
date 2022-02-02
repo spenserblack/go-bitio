e8dc78619cd2cdf6662912f200e4fe4fe0168001
Spenser Black <spenserblack01@gmail.com>
2022-02-02 10:25:56 -0500

    Add example for `Writer`
    Shows how to write and commit bits.

--- example_test.go
+++ example_test.go
@@ -40,3 +40,32 @@ func ExampleWriter_WriteBits() {
 	// 0x34
 	// 0x56
 }
+
+// A Writer writes bits to the underlying writer, but only when a "chunk" is
+// filled. To write a partial chunk (for example, half of a bit), the write
+// must be committed.
+func ExampleWriter() {
+	var buff bytes.Buffer
+
+	// A writer with a chunk size of 2 bytes.
+	w := bitio.NewWriter(&buff, 2)
+
+	// 12 1 bits written. A chunk requires 16 bits (2 bytes), so the write is currently
+	// pending.
+	written, _ := w.WriteBits(0xFFF, 12)
+	fmt.Printf("%d bits written\n", written)
+
+	// Now we commit the pending bits.
+	written, _ = w.CommitPending()
+	fmt.Printf("%d bits written\n", written)
+
+	for i, b := range buff.Bytes() {
+		fmt.Printf("byte %d = 0x%02X\n", i, b)
+	}
+
+	// Output:
+	// 0 bits written
+	// 16 bits written
+	// byte 0 = 0xFF
+	// byte 1 = 0xF0
+}
